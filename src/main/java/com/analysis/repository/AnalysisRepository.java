85fad67ca17f699ff0067915c4e018635eeef71f
Park Chan yong <cksdyd54321@gmail.com>
2026-06-24 12:02:05 +0900

    06.24 거북목 모니터링

--- src/main/java/com/analysis/repository/AnalysisRepository.java
+++ src/main/java/com/analysis/repository/AnalysisRepository.java
@@ -13,6 +13,8 @@ public interface AnalysisRepository extends JpaRepository<Analysis, Long> {
     @Query("SELECT a.id FROM Analysis a WHERE a.memberId = :memberId")
     List<Long> findIdsByMemberId(@Param("memberId") Long memberId);
 
+    
+    //통계 계산 시 기간 경계에 걸친 분석 세션과 진행 중인 분석 세션을 누락하지 않기 위해, memberId와 기간 겹침 조건으로 Analysis를 조회하는 쿼리를 추가
     // startedAt~endedAt이 요청 기간과 겹치는 세션을 조회
     @Query("""
             SELECT a FROM Analysis a
