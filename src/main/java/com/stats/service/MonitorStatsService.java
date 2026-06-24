cab10ee5dcfaa16b4f6b21ab557dd4e906610693
Park Chan yong <cksdyd54321@gmail.com>
2026-06-24 12:40:25 +0900

    06.24 거북목 모니터링

--- src/main/java/com/stats/service/MonitorStatsService.java
+++ src/main/java/com/stats/service/MonitorStatsService.java
@@ -63,8 +63,8 @@ public class MonitorStatsService {
         List<DailyStats> dailyStats =
                 dailyStatsRepository.findByMemberIdAndStatDateBetween(
                         id,
-                        fromDate.atStartOfDay(),
-                        toDate.plusDays(1).atStartOfDay()
+                        fromDate,
+                        toDate
                 );
 
         if (dailyStats.isEmpty()) {
