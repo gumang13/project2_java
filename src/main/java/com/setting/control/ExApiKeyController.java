578705c056e07e4ceb7300d0bc48b1e421974b45
KHK <trafalgalow32@gmail.com>
2026-07-03 15:17:12 +0900

    something deleted

--- src/main/java/com/setting/control/ExApiKeyController.java
+++
@@ -1,5 +0,0 @@
-package com.setting.control;
-
-// 폐기됨: 이미지 분석(/analyze) 및 자세 데이터 목록(/posture-data-list) 엔드포인트는
-// 자세 건강 통계 API(ExPostureStatsController, /api/v1/posture-stats/*)로 대체되었습니다.
-// 이 파일은 git rm 으로 삭제 예정인 빈 플레이스홀더입니다.
